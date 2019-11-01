322512028384efe20590f8062533a8f6d6945795
Maaz <maaznadeem246@gmail.com>
2019-11-02 00:13:36 +0500

    changes in frontend

--- shoppinghub-frontend/src/components/header.js
+++ shoppinghub-frontend/src/components/header.js
@@ -48,7 +48,19 @@ class Header extends Component {
                     <Navbar.Brand  ><span className="webName">Shopping Hub</span></Navbar.Brand>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav" className="navbarrowscss justify-content-end" >
-                        <Nav className="navscss justify-content-end">
+                        <Nav className='navbarsearchform justify-content-end'>
+                            <Form className="navsearchform" >
+                                <FormControl type="text" placeholder="Product Name" className="navseachinput mr-sm-2" />
+                                <Button className="navsearchbutton" >Search</Button>
+                            </Form>
+                            <div className="iconcss">
+                            {
+                                this.state.signedIn && <LinkContainer to="/signin"  ><NavItem className="profile icon"></NavItem></LinkContainer>
+
+                            }
+                            </div>
+                        </Nav>
+                        <Nav className="navscss justify-content-start">
                             <LinkContainer to="/"   ><NavItem className="navitemcss" >Home</NavItem></LinkContainer>
                             <LinkContainer to="/aboutus"   ><NavItem className="navitemcss" >About Us</NavItem></LinkContainer>
                             <LinkContainer to="/contactus"   ><NavItem className="navitemcss" >Contact Us</NavItem></LinkContainer>
@@ -70,12 +82,7 @@ class Header extends Component {
 
                             
                         </Nav>
-                        <Nav className='navbarsearchform justify-content-end'>
-                         <Form className="navsearchform" >
-                            <FormControl type="text" placeholder="Product Name" className="navseachinput mr-sm-2" />
-                            <Button className="navsearchbutton" >Search</Button>
-                        </Form>
-                        </Nav>
+                        
                     </Navbar.Collapse>
                 </Navbar>
             </div> 
