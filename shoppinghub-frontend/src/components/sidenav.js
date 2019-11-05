78a96d832ef685e2e766a9b8f2b2528c4e96ec62
Maaz <maaznadeem246@gmail.com>
2019-11-05 17:52:14 +0500

    dashboard sidenavbar fronend

--- shoppinghub-frontend/src/components/sidenav.js
+++ shoppinghub-frontend/src/components/sidenav.js
@@ -10,27 +10,37 @@ import {Link, BrowserRouter, Route, Switch, withRouter} from "react-router-dom"
 const StyledSideNav = styled.div`
   position: fixed;     /* Fixed Sidebar (stay in place on scroll and position relative to viewport) */
   height: 100%;
-  width: 24%;     /* Set the width of the sidebar */
+ 
   z-index: 1;      /* Stay on top of everything */
   top: 0em;
   left:0em;      /* Stay at the top */
-  background-color: #222; /* Black */
+  background-color: #3b945E; 
   overflow-x: hidden;     /* Disable horizontal scroll */
-  padding-top: 10px;
+  border-top:2px solid #3b945E;
+  border-right:2px solid #3b945E;
+  padding-top:10%;
 `;
 
 
 // css for nav items 'NavItem' component
 const StyledNavItem = styled.div`
-  height: 70px;
-  width: 75px; /* width must be same size as NavBar to center */
-  text-align: center; /* Aligns <a> inside of NavIcon div */
-  margin-bottom: 0;   /* Puts space between NavItems */
+     
+  text-align: left; /* Aligns <a> inside of NavIcon div */
+    width:270px;
+    
   a {
-    font-size: 2.7em;
-    color: ${(props) => props.active ? "white" : "#9FFFCB"};
+    display:block;
+
+    padding-left:10px;
+    font-size: 1.2em;
+    color: ${(props) => !props.active ? "white" : "#3b945E"};
+  
+      background-color:${(props) => !props.active ? "#3b945E" : "white"  };
+        width:inherit;
+    padding-top:7px;
+    padding-bottom:7px;
     :hover {
-      opacity: 0.7;
+      opacity: ${(props) => props.active ? 1 : 0.7  };
       text-decoration: none; /* Gets rid of underlining of icons */
     }  
   }
@@ -40,15 +50,15 @@ const StyledNavItem = styled.div`
 // sidenav items
 class NavItem extends Component{
     handleClick = () => {
-        const {path, onClick} = this.props;
-        onClick(path);
+        const {path, onItemClick} = this.props;
+        onItemClick(path);
     }
     render(){
         const {active} = this.props;
         console.log(this.props)
         return(
             <StyledNavItem active={active}>
-                <Link to={this.props.path} className={this.props.css} onClick={this.handleclick}></Link>
+                <Link to={this.props.path} className={this.props.css} onClick={this.handleClick}>{this.props.name}</Link>
             </StyledNavItem>
         )
     }
@@ -64,14 +74,20 @@ class SideNav extends Component {
                 {
                     path: '/account/profile', /* path is used as id to check which NavItem is active basically */
                     name: 'Profile',
-                    css: 'fa fa-fw fa-home',
+                    css: '',
                     key: 1 /* Key is required, else console throws error. Does this please you Mr. Browser?! */
                 },
                 {
                     path: '/account/dashboard',
                     name: 'Dashboard',
-                    css: 'fa fa-fw fa-clock',
+                    css: '',
                     key: 2
+                }
+                , {
+                    path: '/account/products',
+                    name: 'Products',
+                    css: '',
+                    key: 3
                 }
             ]
         }
@@ -87,19 +103,21 @@ class SideNav extends Component {
         return (
             <StyledSideNav>
                 {
-                 /*  items.map((item) => {
-
+                  items.map((item) => {
+                      console.log(item.path)
+                      console.log(activePath)
+                      console.log(item.path === activePath)
                         return (
-                            { <NavItem 
+                             <NavItem 
                                 path={item.path} 
                                 name={item.name} 
                                 css={item.css} 
                                 onItemClick={this.onItemClick} 
                                 active={item.path === activePath} 
                                 key={item.key}
-                            /> }
+                            /> 
                         ) 
-            })*/
+            })
         }
 
             </StyledSideNav>
